1fa0547f1e537f3c74494b98f9f341cea28c6403
Alejandro Marr <alejandromarr@gmail.com>
2020-03-21 16:37:42 -0300

    Updated swagger documentation.

--- src/routes/category.js
+++ src/routes/category.js
@@ -8,7 +8,15 @@ const { CategoryController } = require('../controllers');
 /**
  * @typedef GetCategoryResponse
  * @property {boolean} success - true/false
+ * @property { Array.<Category> } categories - Categories list
  */
+
+
+/**
+* @typedef Category
+* @property {int} id - Category ID
+* @property {string} name - Category name
+*/
 
 /**
  * @typedef Error
@@ -20,7 +28,7 @@ const { CategoryController } = require('../controllers');
  * Get category endpoint
  * @route GET /api/v1/categories
  * @group Category
- * @returns { Array.<GetCategoryResponse> } 200
+ * @returns { GetCategoryResponse } 200
  * @returns { Error.model }  500 - Unexpected error
  */
 router.get('/', CategoryController.getCategories);
