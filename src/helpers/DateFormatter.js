1afaf2cec0ee5fce9b49c7a83e432fbd56054536
Alejandro Marr <alejandromarr@gmail.com>
2020-03-21 21:01:42 -0300

    Added Swagger documentation for new endpoint.

--- src/helpers/DateFormatter.js
+++ src/helpers/DateFormatter.js
@@ -8,7 +8,6 @@ module.exports = {
   },
 
   formatDate(date) {
-    console.log(date);
     const dateObj = new Date(date);
     const month =  `0${dateObj.getMonth() + 1}`.slice(-2);
     const day = `0${dateObj.getDate()}`.slice(-2);
--- src/routes/category.js
+++ src/routes/category.js
@@ -7,32 +7,59 @@ const { CategoryController } = require('../controllers');
 
 /**
  * @typedef GetCategoryResponse
- * @property {boolean} success - true/false
+ * @property { boolean } success - true
  * @property { Array.<Category> } categories - Categories list
  */
 
+ /**
+ * @typedef GetEventsByCategoryResponse
+ * @property { boolean } success - true
+ * @property { Array.<Event> } events - Events list
+ */
 
 /**
 * @typedef Category
-* @property {int} id - Category ID
-* @property {string} name - Category name
+* @property { int } id - Category ID
+* @property { string } name - Category name
+*/
+
+/**
+* @typedef Event
+* @property { int } id - Event ID
+* @property { string } name - Event name
+* @property { string } image - Event image name
+* @property { string } start_date - Event starting date
+* @property { string } start_time - Event starting time
+* @property { int } duration - Event duration in minutes
+* @property { string } link - Event link
+* @property { int } rating - Rating ID
+* @property { int } category_id
+* @property { string } added - Event creation date
 */
 
 /**
  * @typedef Error
- * @property {boolean} success - true/false
- * @property {string} error - Message error
+ * @property { boolean } success - false
+ * @property { string } error - Message error
  */
 
 /**
  * Get category endpoint
  * @route GET /api/v1/categories
  * @group Category
- * @returns { GetCategoryResponse } 200
+ * @returns { GetCategoryResponse } 200 - OK
  * @returns { Error.model }  500 - Unexpected error
  */
 router.get('/', CategoryController.getCategories);
 
+/**
+ * Get events by category endpoint
+ * @route GET /api/v1/categories/{id}/events
+ * @group Category
+ * @param { integer } id.path.required - Category ID
+ * @returns { GetEventsByCategoryResponse } 200 - OK
+ * @returns { Error.model }  500 - Unexpected error
+ */
 router.get('/:categoryId/events', CategoryController.getEventsByCategory);
 
 module.exports = router;
