199bfd592b2e90d699bd80ddb8edb94fbc7ce4b1
Alejandro Marr <alejandromarr@gmail.com>
2020-03-21 23:23:07 -0300

    Removed fixtures directory.

--- src/fixtures/users.js
+++
@@ -1,8 +0,0 @@
-const createPassword = require('../helpers/create-password');
-
-module.exports = [{
-    username: 'admin',
-    password: createPassword('admin'),
-    name: 'Admin',
-    email: 'admin@example.com',
-}];
